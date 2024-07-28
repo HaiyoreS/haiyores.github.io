455e81dde75ca0a276f53075e29a9bd3b8d9d57b
Доронина Софья <sonicksuperlodovica@gmail.com>
2024-07-28 23:29:07 +0500

    Удален роутинг

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import reportWebVitals from './reportWebVitals'
-import { HashRouter as Router } from "react-router-dom"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Router>
+
         <App />
-    </Router>
+
     
 );
-reportWebVitals();
+
